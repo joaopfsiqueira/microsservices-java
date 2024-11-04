493c2993a2affa1aff49a1f5db772de2f92777de
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2024-11-04 02:10:34 -0300

    feat: gerando documentação

--- src/main/java/br/com/joaopfsiqueira/pedidos/api/controller/PedidoController.java
+++ src/main/java/br/com/joaopfsiqueira/pedidos/api/controller/PedidoController.java
@@ -1,6 +1,11 @@
 package br.com.joaopfsiqueira.pedidos.api.controller;
 
 import br.com.joaopfsiqueira.pedidos.api.entity.Pedido;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -10,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Pedidos", description = "API para pedidos") // Usado para documentar a API
 @RestController // Usado para criar um controlador REST
 @RequestMapping("/api/v1/pedidos") // Usado para mapear solicitações da web para métodos de manipulação de solicitações
 public class PedidoController {
@@ -17,6 +23,10 @@ public class PedidoController {
     // usado para logar algo!
     private final Logger logger = LoggerFactory.getLogger(PedidoController.class);
 
+    @Operation(summary = "Cria um pedido", description = "contem as operações para criar um novo pedido",
+            responses = @ApiResponse(responseCode = "201", description = "Recurso criado com sucesso",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Pedido.class)))) // Usado para documentar a operação
     @PostMapping // Usado para mapear solicitações HTTP POST para métodos de manipulação de solicitações
     public ResponseEntity<Pedido> criarPedido(@RequestBody Pedido pedido) {
         logger.info("Pedido recebido: {}", pedido.toString());
